cbbe7ca43718fb460923133b81612b369c4ba220
fernandoallisson <fernando.alisson@ufpe.br>
2023-10-08 14:35:51 -0300

    Adição de token + endpoint /login

--- src/index.js
+++ src/index.js
@@ -16,9 +16,7 @@ app.get('/', (_request, response) => {
 });
 // ----------------------------
 
-// Endpoint para obter todos os talkers;
-
-app.get('/talker', (req, res) => {
+app.get('/talker', (req, res) => { // endpoint para obter todos os talkers
   fs.readFile(TALKER_PATH_ARCH, (error, data) => {
     if (error) {
       return res.status(500).json({ message: error.message });
@@ -27,15 +25,22 @@ app.get('/talker', (req, res) => {
   });
 });
 
-// Endpoint para obter um talker específico pelo id;
-
-app.get('/talker/:id', (req, res) => {
+app.get('/talker/:id', (req, res) => { // Endpoint para obter um talker específico pelo id;
   const { id } = req.params;
   const talkerById = talker.find((talkers) => talkers.id === Number(id));
   if (!talkerById) {
     return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
   }
   res.status(HTTP_OK_STATUS).json(talkerById);
+});
+
+app.post('/login', (req, res) => { // Endpoint para fazer login. Devolve um token aleatório de 16 caracteres;
+  const { email, password } = req.body;
+  if (email === '' || password === '') {
+    return res.status(401).json({ message: 'Precisa ter alguma coisa no e-mail e senha' });
+  }
+  const token = Math.random().toString(16).substr(2);
+  res.status(HTTP_OK_STATUS).json({ token });
 });
 
 module.exports = {
