52e9d2b4ee0f8627079cfcbf8ff900e21909979d
fernandoallisson <fernando.alisson@ufpe.br>
2023-10-08 13:45:35 -0300

    feat: criação de rota /talker e /talker:id

--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
 const express = require('express');
+const talker = require('./talker.json');
 
 const app = express();
 app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
-const PORT = process.env.PORT || '3001';
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
+// ----------------------------
 
-app.listen(PORT, () => {
-  console.log('Online');
+// Endpoint para obter todos os talkers
+
+app.get('/talker', (req, res) => {
+  res.status(HTTP_OK_STATUS).json(talker);
 });
+
+// Endpoint para obter um talker específico
+
+app.get('/talker/:id', (req, res) => {
+  const { id } = req.params;
+  const talkerById = talker.find((talkers) => talkers.id === Number(id));
+  if (!talkerById) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+  res.status(HTTP_OK_STATUS).json(talkerById);
+});
+
+module.exports = {
+  app,
+};
